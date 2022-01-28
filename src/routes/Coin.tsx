1a16339f2ba389a2a44dd5b6721797141b9c2c8b
Daeyeol Ryu <yoobato@gmail.com>
2022-01-28 23:46:13 +0900

    Add price tab

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -3,6 +3,7 @@ import { Helmet } from "react-helmet";
 import { Link, Outlet, useLocation, useMatch, useParams } from "react-router-dom";
 import styled from "styled-components";
 import { fetchCoinInfo, fetchCoinTickers } from "../api";
+import { PriceData } from "./Price";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -111,40 +112,6 @@ interface InfoData {
   last_data_at: string;
 }
 
-interface PriceData {
-  id: string;
-  name: string;
-  symbol: string;
-  rank: number;
-  circulating_supply: number;
-  total_supply: number;
-  max_supply: number;
-  beta_value: number;
-  first_data_at: string;
-  last_updated: string;
-  quotes: {
-    USD: {
-      ath_date: string;
-      ath_price: number;
-      market_cap: number;
-      market_cap_change_24h: number;
-      percent_change_1h: number;
-      percent_change_1y: number;
-      percent_change_6h: number;
-      percent_change_7d: number;
-      percent_change_12h: number;
-      percent_change_15m: number;
-      percent_change_24h: number;
-      percent_change_30d: number;
-      percent_change_30m: number;
-      percent_from_price_ath: number;
-      price: number;
-      volume_24h: number;
-      volume_24h_change_24h: number;
-    };
-  };
-}
-
 const Coin = () => {
   const { coinId } = useParams();
   const { state } = useLocation() as RouteState;
@@ -229,7 +196,7 @@ const Coin = () => {
           </Tabs>
  
           {/* 자식 Routes를 가지고 온다 */}
-          <Outlet context={{ coinId }} />
+          <Outlet context={{ coinId, tickersData }} />
         </>
       )}
     </Container>
--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,8 +1,99 @@
+import { useOutletContext } from "react-router-dom";
+import styled from "styled-components";
+
+const ListItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+  margin-bottom: 10px;
+  span:last-child {
+    font-weight: 600;
+    color: ${(props) => props.theme.accentColor};
+  }
+`;
+
+export interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
+
+interface PriceProps {
+  tickersData: PriceData
+}
+
 const Price = () => {
-  // TODO: Price 탭 추가
-  
+  const { tickersData } = useOutletContext<PriceProps>();
+
   return (
-    <h1>Price</h1>
+    <>
+      <ListItem>
+        <span>Price changes (last 15 mins)</span>
+        <span>{tickersData.quotes.USD.percent_change_15m}%</span>
+      </ListItem>
+      <ListItem>
+        <span>Price changes (last 30 mins)</span>
+        <span>{tickersData.quotes.USD.percent_change_30m}%</span>
+      </ListItem>
+      <ListItem>
+        <span>Price changes (last 1 hour)</span>
+        <span>{tickersData.quotes.USD.percent_change_1h}%</span>
+      </ListItem>
+      <ListItem>
+        <span>Price changes (last 6 hours)</span>
+        <span>{tickersData.quotes.USD.percent_change_6h}%</span>
+      </ListItem>
+      <ListItem>
+        <span>Price changes (last 12 hours)</span>
+        <span>{tickersData.quotes.USD.percent_change_12h}%</span>
+      </ListItem>
+      <ListItem>
+        <span>Price changes (last 24 hours)</span>
+        <span>{tickersData.quotes.USD.percent_change_24h}%</span>
+      </ListItem>
+      <ListItem>
+        <span>Price changes (last 7 days)</span>
+        <span>{tickersData.quotes.USD.percent_change_7d}%</span>
+      </ListItem>
+      <ListItem>
+        <span>Price changes (last 30 days)</span>
+        <span>{tickersData.quotes.USD.percent_change_30d}%</span>
+      </ListItem>
+      <ListItem>
+        <span>Price changes (last 1 year)</span>
+        <span>{tickersData.quotes.USD.percent_change_1y}%</span>
+      </ListItem>
+    </>
   );
 };
 
